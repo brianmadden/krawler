fd16c3076929b402e973b7405e8fb0deaffb3969
Brian Madden <brian.a.madden@gmail.com>
2017-01-27 08:11:14 -0800

    Import cleaning

--- src/main/kotlin/io/thelandscape/krawler/http/Requests.kt
+++ src/main/kotlin/io/thelandscape/krawler/http/Requests.kt
@@ -34,7 +34,6 @@ import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.DefaultRedirectStrategy
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
-import org.apache.http.protocol.BasicHttpContext
 import org.apache.http.protocol.HttpContext
 import org.apache.http.ssl.SSLContextBuilder
 import java.security.cert.X509Certificate
